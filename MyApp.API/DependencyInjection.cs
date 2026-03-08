3ff37173d2cc6ee5b8ad26a09950b34e5c60fd14
Phulsing <phulsing@deccansoft.net>
2026-03-08 11:23:29 +0530

    first commit

---
+++ MyApp.API/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using MyApp.Application;
+using MyApp.Infrastructure;
+
+namespace MyApp.API
+{
+    public static class  DependencyInjection
+    {
+            public static IServiceCollection AddAppDI(this IServiceCollection service)
+            {
+            service.AddApplicationDI()
+                   .AddInfrastructureDI();
+                return service;
+            }
+       
+    }
+}
